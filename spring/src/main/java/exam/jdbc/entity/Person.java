f3be0f01e306cbdc5299129312e02d251c4e0d14
Rock Lee <rockleeprc@outlook.com>
2017-04-02 17:05:56 +0800

    Spring jdbc API

---
+++ spring/src/main/java/exam/jdbc/entity/Person.java
@@ -0,0 +1,54 @@
+package exam.jdbc.entity;
+
+public class Person {
+	private Integer id;
+	private String name;
+	private int age;
+
+	public Person() {
+		super();
+	}
+
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public Person(Integer id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
