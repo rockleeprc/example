d090e877027a8eb81bc81631a965ce16d9dee90d
rockleeprc <rockleeprc@outlook.com>
2017-07-09 21:02:05 +0800

    edit

--- spring4-mybatis3/src/test/java/exam/service/UserServicTest.java
+++ spring4-mybatis3/src/test/java/exam/service/UserServicTest.java
@@ -1,5 +1,6 @@
 package exam.service;
 
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import exam.pojo.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {  "classpath:spring/applicationContext.xml" })
+@ContextConfiguration(locations = { "classpath:spring/applicationContext.xml" })
 public class UserServicTest {
 	@Autowired
 	IUserService userService;
+	@Autowired
+	SqlSession sqlSession;
+
+	@Test
+	public void sqlSession() {
+		System.out.println(sqlSession);
+	}
 
 	@Test
 	public void select() {
