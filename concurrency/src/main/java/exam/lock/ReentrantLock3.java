08180a726c40764f246256554b3af88595580552
rockleeprc <rockleeprc@outlook.com>
2017-07-09 20:02:39 +0800

    ReentrantLock

---
+++ concurrency/src/main/java/exam/lock/ReentrantLock3.java
@@ -0,0 +1,57 @@
+package exam.lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLock3 {
+
+	public static void main(String[] args) {
+		Lock lock = new ReentrantLock();
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					lock.lock();
+					System.out.println("t1 lock");
+					TimeUnit.SECONDS.sleep(Long.MAX_VALUE);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					lock.unlock();
+				}
+			}
+		}, "t1");
+		t1.start();
+
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					/*
+					 * lock.lock(); 不会响应t2的中断 lockInterruptibly()可以响应中断
+					 */
+					lock.lockInterruptibly();
+					System.out.println("t2 lock");
+					TimeUnit.SECONDS.sleep(5);
+				} catch (InterruptedException e) {
+					System.out.println("t2 interrupt");
+					e.printStackTrace();
+				} finally {
+					lock.unlock();
+				}
+			}
+		}, "t2");
+
+		t2.start();
+
+		try {
+			TimeUnit.SECONDS.sleep(3);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		t2.interrupt();
+	}
+}
