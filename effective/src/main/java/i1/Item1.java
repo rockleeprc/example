dd730ef37c823296f4be34031cb324a2ea01d074
rockleeprc <rockleeprc@outlook.com>
2017-08-06 16:35:28 +0800

    item1

---
+++ effective/src/main/java/i1/Item1.java
@@ -0,0 +1,25 @@
+package i1;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class Item1 {
+	@Test
+	public void t2() {
+		Map<String, List<String>> map = new HashMap<String, List<String>>();
+		Map<String,List<String>> m = MapUtils.newInstance();
+	}
+
+	@Test
+	public void t1() {
+		Boolean t1 = Boolean.valueOf(true);
+		Boolean t2 = Boolean.valueOf(true);
+		System.out.println(t1.hashCode());
+		System.out.println(t2.hashCode());
+		System.out.println(t1 == t2);
+
+	}
+}
