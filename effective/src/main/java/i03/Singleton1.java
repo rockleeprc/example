9f4476e8149dc7adeb8bb43921993cf06778ef1f
yan.li <yan.li@tadu.com>
2017-08-07 20:31:14 +0800

    item3

---
+++ effective/src/main/java/i03/Singleton1.java
@@ -0,0 +1,9 @@
+package i03;
+
+public class Singleton1 {
+	public static final Singleton1 INSTACE = new Singleton1();
+
+	private Singleton1() {
+	}
+
+}
---
+++ effective/src/main/java/i03/Singleton3.java
@@ -0,0 +1,5 @@
+package i03;
+
+public enum Singleton3 {
+	INSTANCE;
+}
