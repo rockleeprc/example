dc3ef647689cbd0c2b658cc2822250a1f3a6cbbc
yan.li <yan.li@tadu.com>
2017-08-12 16:01:08 +0800

    item16

--- effective/src/main/java/i16/B.java
+++ effective/src/main/java/i16/B.java
@@ -2,9 +2,9 @@ package i16;
 
 public class B extends A {
 	/**
-	 * The return type is incompatible with A.method()
+	 * 返回的类型和A.method不一样时，编译无法通过
 	 */
-	public String method() {
-		return "";
+	public /*String*/ void method() {
+		/*return "";*/
 	}
 }
