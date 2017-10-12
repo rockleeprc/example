2de6ef9d116d6b3abe308d7af14bb8aba53800db
yan.li <yan.li@tadu.com>
2017-10-12 16:46:05 +0800

    k17 connection

--- goldentime/src/main/java/k17/keyt/Main.java
+++ goldentime/src/main/java/k17/keyt/Main.java
@@ -3,16 +3,30 @@ package k17.keyt;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class Main {
+	private static final String SECRET_KEY = "MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEA09lLyECaAfz64RM0J/E505rQfxbGTTT9Z/30Tuxbdqc7OwMwX1ZHe5tfxuj/VuaE5UAT3j7xZeijY/HHLijSDwIDAQABAkBRaE8WxLxpxy0hEKAaOThfeD5ml/nb8WDvdUdMjMcY8LdrQVjzNs1wIG3vLA5mRkd336TryBWGpeeyx4mfCWkBAiEA7i5izfmFTiiwzfqqsOroMG6Ds3gG557rjTt/SH+pbLsCIQDjspiI5dGeLGQ/BEDu7T8/VskYZRxgJ1HXIHxKPmfkvQIhAL9DKn6Cl4SK8meFmhoVmLyDkmjEwq6ulDLGi1CZi2DPAiEAyszwJMYknC/HnYTpXKS8d2qRs4Oi8VU0BFpvuSS6HjUCIGljzPDLvBCsOW30vx6PI0IFwuP3TpfBLfowlTlr+1v9";
+
 	public static void main(String[] args) {
-		Map<String,String> param = new HashMap<String,String>();
-		param.put("partnerId", "357");
-		param.put("page", "1");
-		param.put("lastUpdateDate", "2015-12-12 00:00:00");
+		t1();
+	}
+
+	public static void t2() {
+		Map<String, String> param = new HashMap<String, String>();
+		param.put("partnerId", "1021");
+		param.put("bookId", "1721756");
 		String param_str = SignUtil.getParameterString(param);
-		System.out.println("签名结果："+Sign.sign(param_str, "MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAp1XBZSXO4w+iqn5rkSfNt6RXA2VIL4Yi7D+b/Wsoo5AX3Lre1Y5a6dpObSGYMMLoeM3nX1ccCCPeaVbM5EtqjwIDAQABAkEAmYl48vsW8oZ1JeTrg/u2qSrUYBw9eMWmI4lV8texKSlcE8e7mRHOPqm7EicyomNrYTm6eftljZQW60BaGYBIYQIhAPH+BcmHCrw/KstQoEhqSzdnW1/5sd6ZbDDkf7cKVfkfAiEAsQVrTceZzqBvLZNZS2i7TCbrnllYzJPjjhG9lUMssJECIFscfEtTR+x2kAM62Q5KzwUOKi4pkhmwhDaoYZmmD5abAiB1tqDQK/a/TDgiGmW6m35Q+WCc/9To1T7wbiOfafgBwQIgW8WGzfP8uxpCOWbFFXCCKjP1VVzvM7/COF26Ln7vFmU="));
-		
+		System.out.println(param_str);
+		System.out.println("签名结果：" + Sign.sign(param_str, SECRET_KEY));
+	}
+
+	public static void t1() {
+		Map<String, String> param = new HashMap<String, String>();
+		param.put("partnerId", "1021");
+		param.put("page", "1");
+		param.put("lastUpdateDate", "2017-10-08 00:01:01");
+		String param_str = SignUtil.getParameterString(param);
+		System.out.println(param_str);
+		System.out.println("签名结果：" + Sign.sign(param_str, SECRET_KEY));
 	}
 
 }
