693799aff06dbc576a9199d5f2aee0aef4ff876f
yan.li <yan.li@tadu.com>
2017-11-09 11:43:24 +0800

    Reflector

---
+++ mybatis/src/test/java/exam/sourcecode/reflect/ReflectorTest.java
@@ -0,0 +1,53 @@
+package exam.sourcecode.reflect;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import org.apache.ibatis.reflection.DefaultReflectorFactory;
+import org.apache.ibatis.reflection.Reflector;
+import org.apache.ibatis.reflection.ReflectorFactory;
+import org.apache.ibatis.reflection.invoker.Invoker;
+import org.junit.Before;
+import org.junit.Test;
+import exam.mybatis.model.User;
+
+public class ReflectorTest {
+	Reflector userReflector;
+
+	@Before
+	public void init() {
+		ReflectorFactory facotry = new DefaultReflectorFactory();
+		userReflector = facotry.findForClass(User.class);
+	}
+
+	@Test
+	public void testGetterType() {
+		Class<?> clazz = userReflector.getGetterType("userName");
+		System.out.println(clazz.getName());
+	}
+
+	@Test
+	public void testGetInvoker() throws IllegalAccessException, InvocationTargetException, InstantiationException {
+		Invoker invoker = userReflector.getGetInvoker("userName");
+	}
+
+	@Test
+	public void testHasGetter() {
+		System.out.println(userReflector.hasGetter("userName"));
+		System.out.println(userReflector.hasGetter("age"));
+		System.out.println(userReflector.hasGetter("sex"));
+	}
+
+	@Test
+	public void testGetGetablePropertyNames() {
+		String[] propertyNames = userReflector.getGetablePropertyNames();
+		for (String property : propertyNames) {
+			System.out.println(property);
+		}
+	}
+
+	@Test
+	public void testGetDefaultConstructor() {
+		Constructor<?> defaultConstructor = userReflector.getDefaultConstructor();
+		System.out.println(defaultConstructor.getName());
+	}
+}
