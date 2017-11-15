9c09f838ef9e32689388559b70b5ba72367977f7
yan.li <yan.li@tadu.com>
2017-11-15 16:09:35 +0800

    日志测试类

---
+++ spring4-mybatis3/src/test/java/exam/log/Slf4j.java
@@ -0,0 +1,14 @@
+package exam.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Slf4j {
+
+	private static Logger logger = LoggerFactory.getLogger(Slf4j.class);
+
+	public static void main(String[] args) {
+		logger.debug("日志");
+	}
+
+}
--- spring4-mybatis3/src/test/java/exam/service/UserServicTest.java
+++ spring4-mybatis3/src/test/java/exam/service/UserServicTest.java
@@ -1,7 +1,5 @@
 package exam.service;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -20,7 +18,7 @@ public class UserServicTest {
 	@Autowired
 	SqlSessionTemplate sqlSessionTemplate;
 
-	private static final Logger LOG = LogManager.getLogger(UserServicTest.class);
+	//private static final Logger LOG = LogManager.getLogger(UserServicTest.class);
 
 	/*
 	 * log4j2方式 private static final org.apache.logging.log4j.Logger LOG =
@@ -31,7 +29,7 @@ public class UserServicTest {
 	 */
 	@Test
 	public void sqlSession() {
-		LOG.info("aa");
+		//LOG.info("aa");
 		System.out.println(sqlSessionTemplate);
 	}
 
