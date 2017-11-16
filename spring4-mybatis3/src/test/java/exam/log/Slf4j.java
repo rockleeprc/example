c90a7f082bbfd20788adb632a9b8d67edc28c22a
yan.li <yan.li@tadu.com>
2017-11-16 14:52:50 +0800

    edit

--- spring4-mybatis3/src/test/java/exam/log/Slf4j.java
+++ spring4-mybatis3/src/test/java/exam/log/Slf4j.java
@@ -8,7 +8,7 @@ public class Slf4j {
 	private static Logger logger = LoggerFactory.getLogger(Slf4j.class);
 
 	public static void main(String[] args) {
-		logger.debug("日志");
+		logger.debug("日志{} 占位符{}","liyan","sky");
 	}
 
 }
