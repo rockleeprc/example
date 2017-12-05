4aac82f56077bd3956d53b41823ea91121278ba0
yan.li <yan.li@tadu.com>
2017-12-05 19:30:33 +0800

    edit

--- concurrency/src/main/java/exam/interrupt/Interrupt.java
+++ concurrency/src/main/java/exam/interrupt/Interrupt.java
@@ -1,5 +1,7 @@
 package exam.interrupt;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class Interrupt {
@@ -13,13 +15,14 @@ public class Interrupt {
 					System.out.println(System.currentTimeMillis());
 					// 中断逻辑
 					if (Thread.currentThread().isInterrupted()) {
-						System.out.println("线程中断推出");
+						System.out.println("线程中断推出"+Thread.currentThread().isInterrupted());
 						break;
 					}
 					try {
+						System.out.println("中断标记="+Thread.currentThread().isInterrupted());
 						Thread.sleep(3000);
 					} catch (InterruptedException e) {
-						System.out.println("InterruptedException");
+						System.out.println("InterruptedException 中断标记="+Thread.currentThread().isInterrupted());
 						/*
 						 * 在业务逻辑中，为保证数据一致性和完整性，不会立刻退出
 						 * Thread.sleep()由于中断异常，会清除中断标记
@@ -27,6 +30,7 @@ public class Interrupt {
 						 * ()重新设置中断标记
 						 */
 						Thread.currentThread().interrupt();
+						System.out.println("重置中断标记="+Thread.currentThread().isInterrupted());
 					}
 					Thread.yield();
 				}
@@ -35,14 +39,17 @@ public class Interrupt {
 		t.start();
 		Thread.sleep(2000);
 		t.interrupt();
+		System.out.println(t.isInterrupted());
+		
 	}
 
 	/**
 	 * 增加中断逻辑
 	 * @throws InterruptedException
+	 * @throws IOException 
 	 */
 	@Test
-	public void interrupt2() throws InterruptedException {
+	public void interrupt2() throws InterruptedException, IOException {
 		Thread t = new Thread() {
 			@Override
 			public void run() {
@@ -50,7 +57,7 @@ public class Interrupt {
 					System.out.println(System.currentTimeMillis());
 					// 中断逻辑
 					if (Thread.currentThread().isInterrupted()) {
-						System.out.println("线程中断推出");
+						System.out.println("线程中断推出 interrupted="+Thread.currentThread().isInterrupted());
 						break;
 					}
 					Thread.yield();
@@ -60,15 +67,16 @@ public class Interrupt {
 		t.start();
 		Thread.sleep(2000);
 		t.interrupt();
-
+		System.in.read();
 	}
 
 	/**
 	 * 中断不会发生任何作用
 	 * @throws InterruptedException
+	 * @throws IOException 
 	 */
 	@Test
-	public void interrupt1() throws InterruptedException {
+	public void interrupt1() throws InterruptedException, IOException {
 		Thread t = new Thread() {
 			@Override
 			public void run() {
@@ -82,6 +90,7 @@ public class Interrupt {
 		Thread.sleep(2000);
 		// 对t进行中断，t被设置中断状态，但t并没有中断处理逻辑，中断不会发生任何作用
 		t.interrupt();
+		System.in.read();
 	}
 
 }
