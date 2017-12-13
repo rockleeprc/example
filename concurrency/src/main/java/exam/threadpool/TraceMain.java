0db15f9e97e53bd93612db6cf26366b3e0ccd618
yan.li <yan.li@tadu.com>
2017-12-13 17:45:18 +0800

    threadPool

--- concurrency/src/main/java/exam/threadpool/TraceMain.java
+++ concurrency/src/main/java/exam/threadpool/TraceMain.java
@@ -1,6 +1,10 @@
 package exam.threadpool;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class TraceMain {
 	static class DivTask implements Runnable {
@@ -18,10 +22,43 @@ public class TraceMain {
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		// execute();
+		submit();
+	}
+
+	public static void trace() {
 		ThreadPoolExecutor threadPool = new TraceThreadPoolExecutor(10, 10);
 		for (int i = 0; i < 5; i++) {
 			threadPool.execute(new DivTask(100, i));
+		}
+		threadPool.shutdown();
+	}
+
+	/**
+	 * 有异常信息提示
+	 */
+	public static void execute() {
+		ThreadPoolExecutor threadPool = new ThreadPoolExecutor(1, Integer.MAX_VALUE, 0, TimeUnit.MILLISECONDS,
+				new SynchronousQueue<Runnable>());
+		for (int i = 0; i < 5; i++) {
+			threadPool.execute(new DivTask(100, i));
+		}
+		threadPool.shutdown();
+	}
+
+	/**
+	 * submit 提交不会有任何异常提示
+	 * 
+	 * @throws ExecutionException
+	 * @throws InterruptedException
+	 */
+	public static void submit() throws InterruptedException, ExecutionException {
+		ThreadPoolExecutor threadPool = new ThreadPoolExecutor(1, Integer.MAX_VALUE, 0, TimeUnit.MILLISECONDS,
+				new SynchronousQueue<Runnable>());
+		for (int i = 0; i < 5; i++) {
+			Future<?> submit = threadPool.submit(new DivTask(100, i));
+			System.out.println(submit.get());
 		}
 		threadPool.shutdown();
 	}
