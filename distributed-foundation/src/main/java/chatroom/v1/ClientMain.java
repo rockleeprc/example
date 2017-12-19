dfa4d3a50952ae8d4fe8c1ffe4ba36e88166e216
rockleeprc <rockleeprc@outlook.com>
2017-12-19 23:05:58 +0800

    chatroom version 1.0

--- distributed-foundation/src/main/java/chatroom/v1/ClientMain.java
+++ distributed-foundation/src/main/java/chatroom/v1/ClientMain.java
@@ -1,17 +1,32 @@
 package chatroom.v1;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
 
 public class ClientMain {
 
 	public static void main(String[] args) throws IOException {
 		Socket socket = new Socket("127.0.0.1", 9999);
-		OutputStream os = socket.getOutputStream();
-		os.write("aa".getBytes());
+		String line = null;
+		PrintStream print = new PrintStream(socket.getOutputStream());
+		BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+		while((line = buf.readLine())!=null){
+			if("break".equals(line)){
+				//break后,server会阻塞,client收不到任何消息
+				break;
+			}
+			print.print(line);
+		}
 		
-		os.close();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		while((line=reader.readLine())!=null){
+			System.out.println(line);
+		}
+
+		reader.close();
 		socket.close();
 	}
 }
--- distributed-foundation/src/main/java/chatroom/v1/ServerMain.java
+++ distributed-foundation/src/main/java/chatroom/v1/ServerMain.java
@@ -2,6 +2,7 @@ package chatroom.v1;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,12 +12,17 @@ public class ServerMain {
 		ServerSocket serverSocket = new ServerSocket(9999);
 		while (true) {
 			Socket accept = serverSocket.accept();
+			// 调用is.Read()时,如果client没有数据,会引起阻塞
 			InputStream is = accept.getInputStream();
 			byte[] buf = new byte[1024];
 			int len = 0;
 			while ((len = is.read(buf)) != -1) {
 				System.out.println(new String(buf, 0, len));
 			}
+			PrintStream ps = new PrintStream(accept.getOutputStream());
+			ps.print("hi client");
+			ps.close();
+			ps.close();
 		}
 
 	}
