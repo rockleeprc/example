0a68aebe988eec0691c5d7cbcd43700c7608c45d
yan.li <yan.li@tadu.com>
2018-01-19 20:16:49 +0800

    insert无效

--- spring4-mybatis3/src/main/java/exam/dao/UserMapper.java
+++ spring4-mybatis3/src/main/java/exam/dao/UserMapper.java
@@ -7,18 +7,25 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.jarvis.cache.annotation.Cache;
+import com.jarvis.cache.annotation.CacheDelete;
+import com.jarvis.cache.annotation.CacheDeleteKey;
+import com.jarvis.cache.type.CacheOpType;
 
 import exam.pojo.User;
 
 @Repository()
 public interface UserMapper {
-	static final String cacheName = "user";
+	static final String CACHE_NAME = "user_";
+	static final int expire = 600;
 
-	@Cache(expire = 600, autoload = true, key = "'user_mapper_selectByID_'+#args[0]", condition = "#args[0]>0")
+	@Cache(expire = expire, autoload = true, key = "'" + CACHE_NAME + "'+#args[0]", condition = "#args[0]>0")
 	public User selectByID(int id);
 
+	//@Cache(expire = expire, key = "'" + CACHE_NAME + "'+#args[0].id", opType = CacheOpType.WRITE)
+	@Cache(expire=expire, key="'" + CACHE_NAME + "'+#retVal.id", opType=CacheOpType.WRITE)
 	public int insert(User user);
 
+	@CacheDelete({ @CacheDeleteKey(value = "'" + CACHE_NAME + "'+#args[0].id", condition = "null != #args[0]") })
 	public int update(User user);
 
 	public int updateById(User user);
--- spring4-mybatis3/src/test/java/exam/cache/AutoLoadCacheTest.java
+++ spring4-mybatis3/src/test/java/exam/cache/AutoLoadCacheTest.java
@@ -23,10 +23,15 @@ public class AutoLoadCacheTest {
 
 	@Test
 	public void select() throws IOException {
-		User user = userService.selectByID(1);
+		User user = userService.selectByID(19);
 		System.out.println(user);
 	}
 
+	/**
+	 * 查询
+	 * 
+	 * @throws IOException
+	 */
 	@Test
 	public void selectCache() throws IOException {
 		User user = userService.selectByID(1);
@@ -34,6 +39,28 @@ public class AutoLoadCacheTest {
 		user = userService.selectByID(1);
 		System.out.println(user);
 		System.in.read();
+	}
+
+	/**
+	 * 更新
+	 */
+	@Test
+	public void updateCache() {
+		User user = userService.selectByID(1);
+		System.out.println(user);
+		user.setUserName("刘雪娇");
+		int result = userService.update(user);
+		System.out.println(result);
+		user = userService.selectByID(1);
+		System.out.println(user);
+	}
+	
+	@Test
+	public void insetCache(){
+		User u = new User();
+		u.setUserName("bb");
+		int result = userService.insert(u);
+		System.out.println(result);
 	}
 
 }
