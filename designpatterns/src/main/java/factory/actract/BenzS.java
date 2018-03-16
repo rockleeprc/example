da1c6369f4aad142fa665388faabd0e439402971
yan.li <yan.li@tadu.com>
2018-03-16 18:06:22 +0800

    abstract factory

---
+++ designpatterns/src/main/java/factory/actract/BenzS.java
@@ -0,0 +1,10 @@
+package factory.actract;
+
+public class BenzS extends Beanz {
+
+	@Override
+	public String version() {
+		return "S450";
+	}
+
+}
---
+++ designpatterns/src/main/java/factory/actract/Car.java
@@ -0,0 +1,8 @@
+package factory.actract;
+
+public interface Car {
+	public String brand();
+
+	public String version();
+
+}
