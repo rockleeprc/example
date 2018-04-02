07ac5e8f90a3372e2b104c2883b6c48442ad1e42
yan.li <yan.li@tadu.com>
2018-04-02 17:11:36 +0800

    edit

--- concurrency/src/main/java/exam/threadpool/CachedThreadPool.java
+++ concurrency/src/main/java/exam/threadpool/CachedThreadPool.java
@@ -1,5 +1,7 @@
 package exam.threadpool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.SynchronousQueue;
 
 import org.junit.Test;
@@ -8,8 +10,27 @@ public class CachedThreadPool {
 
 	
 	@Test
-	public void testSyn() throws InterruptedException{
+	public void testSynchronousQueue() throws InterruptedException{
 		SynchronousQueue<Integer> queue = new SynchronousQueue<>();
 		System.out.println(queue.offer(1));
+	}
+	
+	@Test
+	public void testnewCachedThreadPool() throws InterruptedException{
+		ExecutorService threadPool = Executors.newCachedThreadPool();
+		threadPool.execute(new Task());
+		threadPool.execute(new Task());
+	}
+	
+	public static class Task implements Runnable {
+		@Override
+		public void run() {
+			System.out.println(System.currentTimeMillis() + " Thread ID:" + Thread.currentThread().getId());
+			try {
+				Thread.sleep(Integer.MAX_VALUE);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
