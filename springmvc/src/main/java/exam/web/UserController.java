6e39e240d281aa4045e404913c6ec55b207f6ee3
Rock Lee <rockleeprc@outlook.com>
2018-06-18 21:58:31 +0800

    spring

--- springmvc/src/main/java/exam/web/UserController.java
+++ springmvc/src/main/java/exam/web/UserController.java
@@ -1,23 +1,33 @@
 package exam.web;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 import exam.domain.User;
 
-
 @Controller
 @RequestMapping("/user")
-public class UserController {
+public class UserController implements EnvironmentAware {
+
+	private Environment environment;
 
 	@RequestMapping("/register")
-	public String register() {
+	public String register(HttpServletRequest reqeust) {
+		/**
+		 * org.springframework.web.servlet.FrameworkServlet.initWebApplicationContext()
+		 * WebApplicationContext wac = (WebApplicationContext) reqeust.getServletContext().getAttribute("org.springframework.web.servlet.FrameworkServlet.CONTEXT.dispatcherServlet");
+		 */
 		return "user/register";
 	}
 
-	@RequestMapping(value="/add",method=RequestMethod.POST)
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public ModelAndView add(User user) {
 		System.out.println(user);
 		ModelAndView mav = new ModelAndView();
@@ -25,5 +35,10 @@ public class UserController {
 		mav.addObject("user", user);
 		return mav;
 
+	}
+
+	@Override
+	public void setEnvironment(Environment environment) {
+		this.environment = environment;
 	}
 }
