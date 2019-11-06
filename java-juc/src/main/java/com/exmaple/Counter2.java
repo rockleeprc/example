32517afd16c87cf4002beb361751f3975cbb9c6a
rockleeprc <rockleeprc@outlook.com>
2019-11-06 11:34:14 +0800

    线程间通信

---
+++ java-juc/src/main/java/com/exmaple/Counter2.java
@@ -0,0 +1,60 @@
+package com.exmaple;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Counter2 {
+    static volatile List<Object> list = new ArrayList<>();
+    static Object lock = new Object();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (lock) {
+                System.out.println("t2 start");
+                if (list.size() != 5) {
+                    try {
+                        lock.wait();// 释放锁
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("t2 ending");
+                lock.notify();// 通知t1
+            }
+        }, "t2").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            synchronized (lock) {
+                System.out.println("t1 start");
+                for (int i = 0; i < 10; i++) {
+                    list.add(i);
+                    System.out.println(i);
+                    if (i == 5) {
+                        lock.notify();
+
+                        try {
+                            lock.wait();// 释放锁 让t2运行
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("t1 ending");
+            }
+        }, "t1").start();
+    }
+}
---
+++ java-juc/src/main/java/com/exmaple/Counter3.java
@@ -0,0 +1,46 @@
+package com.exmaple;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class Counter3 {
+    static volatile List<Object> list = new ArrayList<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        new Thread(() -> {
+            System.out.println("t2 start");
+            if (list.size() != 5) {
+                try {
+                    latch.await();// 等待t1 count down
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("t2 ending");
+        }, "t2").start();
+
+        TimeUnit.SECONDS.sleep(1);
+
+        new Thread(() -> {
+            System.out.println("t1 start");
+            for (int i = 0; i < 10; i++) {
+                list.add(i);
+                System.out.println(i);
+                if (list.size() == 5) {
+                    latch.countDown();
+                }
+
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("t1 ending");
+        }, "t1").start();
+    }
+}
