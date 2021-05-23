a962f571b93a717807e37c92a2b06d0cf7d32a4d
liyan <rockleeprc@outlook.com>
2021-05-23 23:03:39 +0800

    ed

--- java-juc/src/main/java/org/example/StringTable.java
+++ java-juc/src/main/java/org/example/StringTable.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
+
 public class StringTable {
     public static void main(String[] args) {
-        
+        ByteBuffer allocate = ByteBuffer.allocateDirect(1024 * 1024 * 1);
+
+        Unsafe unsafe = getUnsafe();
+    }
+
+    public static Unsafe getUnsafe() {
+        try {
+            Field f = Unsafe.class.getDeclaredField("theUnsafe");
+            f.setAccessible(true);
+            Unsafe unsafe = (Unsafe) f.get(null);
+            return unsafe;
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
